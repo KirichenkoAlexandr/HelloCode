2edba2738b59cbab8357687d806df8e04fb4ddf6
KirichenkoAlexandr <magnetta1990@gmail.com>
2023-05-18 17:45:19 +0300

    add else

---
+++ example005_conditionsElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mary")
+{
+    Console.WriteLine("hello lovely Mary");
+}
+else
+{
+    Console.WriteLine("hello");
+    Console.WriteLine(username);
+}
